c5b9b0ebe8e6ff367af9a0e65869a89b198df28f
Nabin <nkbhaie2017@gmail.com>
2021-03-10 09:47:39 -0700

    python files set up

---
+++ count_up.py
@@ -0,0 +1,20 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop."""
+    for count in range(start, stop+1):
+        print(count)
+count_up(5, 7)        
